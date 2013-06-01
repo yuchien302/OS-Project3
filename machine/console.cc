9845ba5b6e16e05924497fe6024575a1fb7c422b
Raphael <raphaeltnfsh@gmail.com>
2013-06-02 01:35:47 +0800

    Implementing

--- machine/console.cc
+++ machine/console.cc
@@ -178,7 +178,7 @@ void
 ConsoleOutput::PutString(std::string str)
 {
     ASSERT(putBusy == FALSE);
-    WriteFile(writeFileNo, &(str.c_str()), sizeof(str));
+    WriteFile(writeFileNo, (str.c_str()), sizeof(str));
     putBusy = TRUE;
     kernel->interrupt->Schedule(this, ConsoleTime, ConsoleWriteInt);
 }
