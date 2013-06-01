098c26b5ec968d7781e1f0f5907763d1d6e34d5c
Raphael <raphaeltnfsh@gmail.com>
2013-06-02 01:21:41 +0800

    Implementing

--- userprog/ksyscall.h
+++ userprog/ksyscall.h
@@ -31,14 +31,16 @@ int SysPrint(char* input, int num){
 
   int data[1];
   int i=0;
+  kernel->synchConsoleOut->PutChar('l');
+  kernel->synchConsoleOut->PutChar('a');
+  kernel->synchConsoleOut->PutChar('\n');
+
   do {
     kernel->machine->ReadMem((int)input+i, 1, data);
     kernel->synchConsoleOut->PutChar(data[0]);
     i++;
   } while(data[0]!='\0');
-  kernel->synchConsoleOut->PutChar('l');
-  kernel->synchConsoleOut->PutChar('a');
-  kernel->synchConsoleOut->PutChar('\n');
+
   return i;
 }
 
