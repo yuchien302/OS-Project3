eea43fefd20ae97a1ba97c1fa8728a7ba6269950
Raphael <raphaeltnfsh@gmail.com>
2013-06-02 10:44:15 +0800

    Implementing

--- test/a.c
+++ test/a.c
@@ -1,11 +1,12 @@
 #include <syscall.h>
 
 int main(){
-	int i;
-	for( i=1;i<5;i++){
-		OSPrint("%da001\n", i);
-		OSPrint("%da0-1\n", i);
-		OSPrint("%da002\n", i);
+	// int i;
+	// for( i=1;i<5;i++){
+		OSPrint("%da001\n", 1);
+		OSPrint("%da001\n", 2);
+		// OSPrint("%da0-1\n", i);
+		// OSPrint("%da002\n", i);
 		
-	}
+	// }
 }
